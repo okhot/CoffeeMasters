b32bf0cc5687ce39e0232a39c260088dc3a6dc66
Arrey-Etta <33989423+okhot@users.noreply.github.com>
2024-01-25 23:34:50 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Arrey-Etta on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
